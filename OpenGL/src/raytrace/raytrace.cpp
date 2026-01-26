51502b45a37ade24caaf4d538ea07feac1b7202d
Benjamin Hurley <74065746+ItsBentacular@users.noreply.github.com>
2026-01-26 17:41:57 -0600

    finished some of the raytracer, pretty awesome how PPM files work, hopefully the books that mom and dad got me are worth it, even though they won't see this I really do appreciate what they do for me to let me follow my dreams, anywho time to go to work.

--- OpenGL/src/raytrace/raytrace.cpp
+++ OpenGL/src/raytrace/raytrace.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "vec3.h"
+#include "color.h"
 
 int main()
 {
@@ -7,10 +8,6 @@ int main()
 
     int image_width = 256;
     int image_height = 256;
-    vec3 point1(1.0,1.0,1.0);
-    vec3 point2(3.0,1.0,1.0);
-    vec3 point3(2.0,3.0,1.0);
-
 
     //Render
 
@@ -19,15 +16,8 @@ int main()
     for(int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int (255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), double(100) / (image_height - 1));
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone.           \n";
